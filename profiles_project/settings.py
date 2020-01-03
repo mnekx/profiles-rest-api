3725fe62b91470b07a5c85d9fb629d4c0153a304
Mnemba Chambuya <karimnembac@gmail.com>
2020-01-03 13:12:53 +0300

    changed database to postgres

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -73,13 +73,20 @@ TEMPLATES = [
 WSGI_APPLICATION = 'profiles_project.wsgi.application'
 
 
-# Database
+# Database 
+#NAME test
+#username: mnex
+#password: ''
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'test',
+        'USER': 'mnex',
+        'PASSWORD': '123',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
 
