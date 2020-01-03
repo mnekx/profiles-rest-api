b632e28408d7f23dac76b6e9155890ac46f33f69
Mnemba Chambuya <karimnembac@gmail.com>
2020-01-03 17:09:30 +0300

    added 'st-take-api.herokuapp.com' to allowed hosts

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # DEBUG = True
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['st-take-api.herokuapp.com']
 
 
 # Application definition
